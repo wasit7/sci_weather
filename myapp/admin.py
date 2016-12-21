c98b80c77a724b7deca1819c5416dc1c7e0028ea
Krerkkiat Chusap <chusap.krerkkiat@gmail.com>
2016-12-21 03:32:38 -0500

    Clean repo. Clean *.pyc. Add .gitignore. Add CDN for DataTable and jQuery.

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Weather
+
+# Register your models here
+class WeatherAdmin(admin.ModelAdmin):
+    list_display = ('time', 'nodeid', 'temp', 'humi', 'israin')
+
 # Register your models here.
+admin.site.register(Weather, WeatherAdmin)
